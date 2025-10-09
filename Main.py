0f6036816a9bdeee68b2b34ba59aaa278aeb5823
tomasrothfe-rgh <tomasrothfe@gmail.com>
2025-10-08 23:32:47 -0300

    Descripción del cambio

--- Main.py
+++ Main.py
@@ -7,11 +7,12 @@ ventana.geometry("800x600")
 ventana.configure(bg="#181818")
 
 # BLOQUE IZQUIERDO
-bloque_izquierdo = tk.Frame(ventana, bg="#1f1f1f", width=250, bd=1, highlightbackground="#2b2b2b", highlightthickness=1)
-bloque_izquierdo.pack(side="left", fill="y")
-imagen_logo = tk.PhotoImage(file="BIBLIOTECA_PERSONAL\logo-mibi.png").subsample(3, 3) 
+bloque_izquierdo = tk.Frame(ventana, bg="#1f1f1f", width=300, bd=1, highlightbackground="#2b2b2b", highlightthickness=1)
+bloque_izquierdo.pack(side="left", fill="y", padx=6, pady=6)
+bloque_izquierdo.pack_propagate(False)
+imagen_logo = tk.PhotoImage(file="BIBLIOTECA_PERSONAL\logo-mibi.png").subsample(4, 4) 
 logo = tk.Label(bloque_izquierdo, image=imagen_logo, bg="#1f1f1f")
-logo.pack(side="top", anchor="n", pady=10, padx=10)
+logo.pack(side="left", anchor="n", pady=10, padx=10)
 
 
 
