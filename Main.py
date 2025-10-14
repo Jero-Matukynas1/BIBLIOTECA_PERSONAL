a8f6900d9595bebb99096b0de1d9b08ecbbe9997
tomasrothfe-rgh <tomasrothfe@gmail.com>
2025-10-14 15:32:58 -0300

    Faltaria combinar los dos codigos y lo de los filtros

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ ventana.configure(bg="#181818")
 bloque_izquierdo = tk.Frame(ventana, bg="#1f1f1f", width=300, bd=1, highlightbackground="#2b2b2b", highlightthickness=1)
 bloque_izquierdo.pack(side="left", fill="y", padx=6, pady=6)
 bloque_izquierdo.pack_propagate(False)
-imagen_logo = tk.PhotoImage(file=r"C:\Users\JERONIMO\OneDrive\Escritorio\BIB_ITEC\logo-mibi.png").subsample(4, 4)
+imagen_logo = tk.PhotoImage(file=r"dfdsf\BIBLIOTECA_PERSONAL\logo-mibi.png").subsample(4, 4)
 logo = tk.Label(bloque_izquierdo, image=imagen_logo, bg="#1f1f1f")
 logo.pack(side="left", anchor="n", pady=10, padx=10)
 
